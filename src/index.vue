2284cf19e1aee741ea4ba5839170cdd25904fbdb
hanqing <229075284@qq.com>
2020-11-05 19:41:05 +0800

    init

---
+++ src/index.vue
@@ -0,0 +1,14 @@
+<template>
+<div>1</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+}
+</script>
+<style scoped>
+
+</style>
---
+++ vue.js
@@ -0,0 +1,39 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      // "jsx": true
+    },
+    // debugLevel:true,
+    // 用于eslint-config-airbnb-typescript
+    // project: './tsconfig.json',
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  // plugins: [
+  //   'vue',
+  // ],
+  extends: [
+    'airbnb-base',
+    'plugin:vue/essential',
+    '@vue/typescript/recommended',
+  ],
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    'no-console': 'warn',
+    "semi":['error',"never"]
+  },
+  "overrides": [
+    {
+      // https://github.com/typescript-eslint/typescript-eslint/blob/v4.6.1/packages/eslint-plugin/docs/rules/explicit-module-boundary-types.md#configuring-in-a-mixed-jsts-codebase
+      // enable the rule specifically for TypeScript files
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        "@typescript-eslint/explicit-module-boundary-types": ["error"]
+      }
+    }
+  ]
+};
