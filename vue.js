6b6903d42af1283df82a1725232eab2fdf7b633a
hanqing <229075284@qq.com>
2020-11-06 17:06:28 +0800

    update

--- vue.js
+++ vue.js
@@ -2,9 +2,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 6,
     sourceType: 'module',
-    ecmaFeatures: {
-      // "jsx": true
-    },
+    ecmaFeatures: { /* jsx: true */ },
     // debugLevel:true,
     // 用于eslint-config-airbnb-typescript
     // project: './tsconfig.json',
@@ -26,15 +24,17 @@ module.exports = {
     'no-console': 'warn',
     semi: ['error', 'never'],
     'max-lines': ['error', { max: 600, skipBlankLines: true, skipComments: true }],
+    // 'multiline-comment-style': ['error', 'starred-block'],
+    'operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
+    'object-curly-newline': ['error', { multiline: true, minProperties: 6 }],
+    'id-length': ['error', { min: 4, max: 20, properties: 'never', exceptionPatterns: ['[x-z]'] }],
   },
   overrides: [
     {
       // https://github.com/typescript-eslint/typescript-eslint/blob/v4.6.1/packages/eslint-plugin/docs/rules/explicit-module-boundary-types.md#configuring-in-a-mixed-jsts-codebase
       // enable the rule specifically for TypeScript files
       files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/explicit-module-boundary-types': ['error'],
-      },
+      rules: { '@typescript-eslint/explicit-module-boundary-types': ['error'] },
     },
   ],
 }
