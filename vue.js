850c553683ad780614e29c799b1d558d49d7a156
hanqing <229075284@qq.com>
2021-04-25 14:56:10 +0800

    update

--- vue.js
+++ vue.js
@@ -27,14 +27,26 @@ module.exports = {
     // 'multiline-comment-style': ['error', 'starred-block'],
     'operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
     'object-curly-newline': ['error', { multiline: true, minProperties: 6 }],
-    'id-length': ['error', { min: 4, max: 20, properties: 'never', exceptionPatterns: ['[x-z]'] }],
+    'id-length': ['error', { min: 3, max: 20, exceptions: ['e', 'to', 'from'] }],
+    'max-len': ['error', { code: 136, comments: 100 }],
+    'no-use-before-define': ['error', { functions: false }],
   },
   overrides: [
     {
+      // eslint-disable-nextline
       // https://github.com/typescript-eslint/typescript-eslint/blob/v4.6.1/packages/eslint-plugin/docs/rules/explicit-module-boundary-types.md#configuring-in-a-mixed-jsts-codebase
       // enable the rule specifically for TypeScript files
       files: ['*.ts', '*.tsx'],
       rules: { '@typescript-eslint/explicit-module-boundary-types': ['error'] },
     },
   ],
+  // // settings参考：https://github.com/vuejs/eslint-config-airbnb/blob/master/index.js
+  // settings: {
+  //   'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx'],
+  //   'import/resolver': {
+  //     // https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-511007063
+  //     // node: {},
+  //     webpack: { config: require.resolve('@vue/cli-service/webpack.config.js') },
+  //   },
+  // },
 }
