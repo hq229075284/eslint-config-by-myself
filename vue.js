b19af4359fe4d531c035ab2bcd6c5ba8fe8b3466
hanqing <hanq@qq.com>
2021-05-19 17:16:25 +0800

    添加允许的短命名

--- vue.js
+++ vue.js
@@ -29,7 +29,7 @@ module.exports = {
     // 'multiline-comment-style': ['error', 'starred-block'],
     'operator-linebreak': ['error', 'after', { overrides: { '?': 'before', ':': 'before' } }],
     'object-curly-newline': ['error', { multiline: true, minProperties: 6 }],
-    'id-length': ['error', { min: 3, max: 28, exceptions: ['ol', 'd', 'ua', 'UA', 'x', 'y', 'e', 'to', 'h', 'dd', 'v', 'from'] }],
+    'id-length': ['error', { min: 3, max: 28, exceptions: ['id', 'ol', 'd', 'ua', 'UA', 'x', 'y', 'e', 'to', 'h', 'dd', 'v', 'i', 'fn', 'from'] }],
     'max-len': ['error', { code: 148, comments: 100 }],
     'no-use-before-define': ['error', { functions: false }],
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
@@ -77,34 +77,10 @@ module.exports = {
         '@typescript-eslint/explicit-module-boundary-types': ['error'],
         // https://github.com/typescript-eslint/typescript-eslint/blob/v4.22.0/packages/eslint-plugin/docs/rules/ban-ts-comment.md#rule-details
         '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': false }],
-        // // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/indent.md#how-to-use
-        // indent: 'off',
-        // // https://github.com/airbnb/javascript/blob/1eadb93e377da1e56c3f91f26610e5d0a00738a9/packages/eslint-config-airbnb-base/rules/style.js#L130
-        // '@typescript-eslint/indent': ['error', 2, {
-        //   SwitchCase: 1,
-        //   VariableDeclarator: 1,
-        //   outerIIFEBody: 1,
-        //   // MemberExpression: null,
-        //   FunctionDeclaration: {
-        //     parameters: 1,
-        //     body: 1,
-        //   },
-        //   FunctionExpression: {
-        //     parameters: 1,
-        //     body: 1,
-        //   },
-        //   CallExpression: { arguments: 1 },
-        //   ArrayExpression: 1,
-        //   ObjectExpression: 1,
-        //   ImportDeclaration: 1,
-        //   flatTernaryExpressions: false,
-        //   // list derived from https://github.com/benjamn/ast-types/blob/HEAD/def/jsx.js
-        //   ignoredNodes: ['JSXElement', 'JSXElement > *', 'JSXAttribute', 'JSXIdentifier', 'JSXNamespacedName', 'JSXMemberExpression', 'JSXSpreadAttribute', 'JSXExpressionContainer', 'JSXOpeningElement', 'JSXClosingElement', 'JSXFragment', 'JSXOpeningFragment', 'JSXClosingFragment', 'JSXText', 'JSXEmptyExpression', 'JSXSpreadChild'],
-        //   ignoreComments: false,
-        // }],
       },
     },
   ],
+  // 以下在项目中配置
   // // settings参考：https://github.com/vuejs/eslint-config-airbnb/blob/master/index.js
   // settings: {
   //   'import/extensions': ['.js', '.ts', '.mjs', '.jsx', '.tsx'],
